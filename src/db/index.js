f617551eeaf8fd0f053d140c455847c674bd940f
M. Mutahar <mmutahar38@gamil.com>
2024-01-24 23:17:01 +0500

    connect db and password hasd in main (index file)

---
+++ src/db/index.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+import { DB_Name } from "../constants.js";
+// import { SignupUsersSchema } from "../models/signupusers.modesl.js";
+import SignupSchema from "../models/signupusers.modesl.js";
+// const signupSchema=new mongoose.Schema(
+//     {
+//         // fname:{
+//         //     type : String,
+//         //     required : true,
+//         // },
+//         // lname:{
+//         //     type : String,
+//         //     required : true,
+//         // },
+//         // email:{
+//         //     type : String,
+//         //     required : true
+//         // },
+//         // password:{
+//         //     type : String,
+//         //     required : true
+//         // }
+//         message :[{
+//             type : String,
+//             required : true
+//         }]
+//     },
+//     {timestamps : true})
+
+
+const MURI="mongodb://localhost:27017/Shopping_pro" 
+const MRI=" mongodb://127.0.0.1:27017/"
+const connectDB = async (data)=>{
+    const newData=data;
+    try{
+const conectionInstance=await mongoose.connect(`${MRI}/${DB_Name}`)
+console.log("mongodb connected Succefully")
+// console.log(conectionInstance.connection.host)  
+console.log("data",data)
+
+// newData.save().then(item => res.send(item)).catch(err => res.status(400).send(err));
+}
+catch(error){
+    console.log("Error",error);
+}
+}
+
+
+
+
+
+export const SignupUser=mongoose.model("signupUser",SignupSchema);
+export default connectDB
